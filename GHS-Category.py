acf218759ec6052463f4df17070c17784e988d00
UbeTake <takeshiro.ube@gmail.com>
2024-08-03 12:39:52 +0900

    1st commit

---
+++ GHS-Category.py
@@ -0,0 +1,33 @@
+import streamlit as st
+from PIL import Image
+
+# タイトルを表示
+st.title("自然環境汚染")
+# st.write('# headline1')
+# st.markdown('# headline2')
+
+# ローカルの画像ファイルを読み込む
+image = Image.open("C:/Users/User/anaconda3/ube/GHS-pictogram/GHS-pictogram-Nature_polluting.png")
+
+# 画像を30x30ピクセルにリサイズ
+# resized_image = image.resize((30, 30))
+# 画像を表示
+# st.image(resized_image, caption="自然環境汚染", use_column_width=False)
+
+# 高解像度のまま画像を表示
+# st.image(image, caption="自然環境汚染", width=150)
+st.image(image, width=150)
+
+st.subheader('■リスク情報')
+st.text('・水生生物に非常に強い毒性')
+
+st.subheader('■安全対策')
+st.text('・環境への放出を避けること。')
+st.text('・内容物/容器を法令にしたがって廃棄すること。')
+
+st.subheader('■応急措置')
+st.text('・火災の場合：適切な消火剤又は水を用いて消火すること。')
+st.text('・飲み込んだ場合：ただちに医師に連絡すること。口をすすぐこと。')
+st.text('・眼に入った場合：水で数分間注意深く洗うこと。コンタクトレンズを使用していて容易に外せる場合は外すこと。その後も洗浄を続けること。ただちに医師に連絡すること。')
+st.text('・皮膚等に付着した場合：ただちに汚染された衣類を全て脱ぐこと。皮膚を多量の水と石けんで洗うこと。皮膚刺激が生じた場合、医師の手当てを受けること。')
+
