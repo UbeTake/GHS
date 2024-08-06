3a81e539ebb25439f8e50236c851081dd8b88458
UbeTake <takeshiro.ube@gmail.com>
2024-08-06 14:57:41 +0900

    1st commit

--- GHS-Category.py
+++ GHS-Category.py
@@ -1,11 +1,15 @@
+import streamlit as st
 import numpy as np
 import streamlit as st
 import tensorflow as tf
 from PIL import Image
+from tensorflow.keras.preprocessing.image import load_img
 
 # カメラ
 picture = st.camera_input("Take a picture")
 img_path = "./sample.jpg"
+# 画像のアップロード
+uploaded_file = st.file_uploader("画像をアップロードしてください", type=["jpg", "png"])
 
 # 画像のクラス名
 classes = ['Corrosive',
@@ -18,7 +22,10 @@ classes = ['Corrosive',
            'Oxidizing',
            'Toxic']
 
-if img_path is not None:
+if uploaded_file is not None:
+    image = load_img(uploaded_file, target_size=(224, 224))
+    st.image(image, caption='アップロードされた画像。', use_column_width=True)
+
     # 画像の読み込みと前処理
     img = Image.open(img_path)
     img = img.resize((150, 150))
@@ -30,12 +37,14 @@ if img_path is not None:
     model = tf.keras.models.load_model('./my_model.h5')
 
     # 画像の分類
-    # predictions = model.predict(x)
-    # predicted_class = np.argmax(predictions[0])
-    # predicted_label = classes[predicted_class]
-    # st.write(f'Predicted class: {predicted_class}')
-    # st.write(f'Predicted label: {predicted_label}')
+    st.write("分類処理中...")
+    predictions = model.predict(x)
+    predicted_class = np.argmax(predictions[0])
+    predicted_label = classes[predicted_class]
+    st.write(f'Predicted class: {predicted_class}')
+    st.write(f'Predicted label: {predicted_label}')
+    st.write("分類結果:", predictions)
     # 画像の表示
     st.image(img_path, width=150)
     # 結果の表示
-    # print(f'This image is classified as: {predicted_label}')
+    print(f'This image is classified as: {predicted_label}')
